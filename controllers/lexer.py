c01793cc1e2c8aeb6f84779e09d5e46a8c1cf543
fherbine <felix.herbinet@yahoo.com>
2019-10-11 13:50:17 +0200

    Lexer

---
+++ controllers/lexer.py
@@ -0,0 +1,30 @@
+from sly import Lexer
+
+class PolyLexer(Lexer):
+    tokens = { NUMBER, ADD, MINUS, TIMES, DIVIDE, LPAREN,
+               RPAREN, EQ, X, POWER }
+
+
+    ignore = ' \t'
+
+    NUMBER = '\d*\.?\d+'
+    ADD = r'\+'
+    MINUS = r'-'
+    TIMES = r'\*'
+    DIVIDE = r'/'
+    LPAREN = r'[\(\[]'
+    RPAREN = r'[\)\]]'
+    EQ = r'='
+    X = r'X\^[0-9]+'
+    POWER = r'\^'
+
+    def NUMBER(self, token):
+        if '.' in token.value:
+            token.value = float(token.value)
+        else:
+            token.value = int(token.value)
+        return token
+
+    def error(self, token):
+        print('Illegal character: `%s`' % token.value[0])
+        self.index += 1
